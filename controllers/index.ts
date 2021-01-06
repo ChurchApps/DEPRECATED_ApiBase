34e499df504517a330899746570d888c5d133f7f
Jeremy Zongker <jeremy@trilitech.com>
2021-01-06 01:36:51 -0600

    Moved CustomBaseController to ApiBase

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,3 +1,4 @@
+export { CustomBaseController } from "./CustomBaseController"
 export { ErrorController } from "./ErrorController";
 export { FormController } from "./FormController";
 export { FormSubmissionController } from "./FormSubmissionController";
