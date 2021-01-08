16ec2c5b46d70e304bdd87e0b6ae8eb1bb025c3c
Jeremy Zongker <jeremy@trilitech.com>
2021-01-08 00:34:37 -0600

    Added pages and links

--- repositories/index.ts
+++ repositories/index.ts
@@ -1,6 +1,8 @@
 export { AnswerRepository } from "./AnswerRepository";
 export { FormRepository } from "./FormRepository";
 export { FormSubmissionRepository } from "./FormSubmissionRepository";
+export { LinkRepository } from "./LinkRepository";
 export { NoteRepository } from "./NoteRepository";
 export { QuestionRepository } from "./QuestionRepository";
+export { PageRepository } from "./PageRepository";
 export { Repositories } from "./Repositories";
