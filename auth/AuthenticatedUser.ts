747889d7ca637bba08407fb10d7e7db876e6ed3b
Jeremy Zongker <jeremy@trilitech.com>
2021-01-18 00:13:51 -0600

    Updated appName to apiName

--- auth/AuthenticatedUser.ts
+++ auth/AuthenticatedUser.ts
@@ -1,20 +1,18 @@
 import { Principal } from './'
-import jwt from "jsonwebtoken";
-import { Repositories } from '../repositories';
 
 export class AuthenticatedUser {
     // public details: any;
     public id: number;
     public churchId: number;
     public email: string;
-    public appName: string;
+    public apiName: string;
     public permissions: string[];
 
     public constructor(principal: Principal) {
         this.id = principal.details.id;
         this.churchId = principal.details.churchId;
         this.permissions = principal.details.permissions;
-        this.appName = principal.details.appName;
+        this.apiName = principal.details.apiName;
     }
 
     public checkAccess(contentType: string, action: string) {
