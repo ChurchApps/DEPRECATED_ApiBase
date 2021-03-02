2c3cabd863cfc3e45948191aa14e75cd86d2fc99
pranavmalvawala <pranavmalvawala@gmail.com>
2021-03-02 23:08:01 +0530

    Add church appearence fields to Table and update API accordingly

--- repositories/SettingRepository.ts
+++ repositories/SettingRepository.ts
@@ -13,15 +13,15 @@ export class SettingRepository {
     public async create(setting: Setting) {
         setting.id = UniqueIdHelper.shortId();
         return DB.query(
-            "INSERT INTO settings (id, churchId, keyName, value) VALUES (?, ?, ?, ?)",
-            [setting.id, setting.churchId, setting.keyName, setting.value]
+            "INSERT INTO settings (id, churchId, keyName, value, homePageUrl, logoUrl, primaryColor, contrastColor) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            [setting.id, setting.churchId, setting.keyName, setting.value, setting.homePageUrl, setting.logoUrl, setting.primaryColor, setting.contrastColor]
         ).then(() => { return setting; });
     }
 
     public async update(setting: Setting) {
         return DB.query(
-            "UPDATE settings SET churchId=?, keyName=?, value=? WHERE id=? AND churchId=?",
-            [setting.churchId, setting.keyName, setting.value, setting.id, setting.churchId]
+            "UPDATE settings SET churchId=?, keyName=?, value=?, homePageUrl=?, logoUrl=?, primaryColor=?, contrastColor=? WHERE id=? AND churchId=?",
+            [setting.churchId, setting.keyName, setting.value, setting.homePageUrl, setting.logoUrl, setting.primaryColor, setting.contrastColor, setting.id, setting.churchId]
         ).then(() => setting)
     }
 
@@ -33,7 +33,11 @@ export class SettingRepository {
         const result: Setting = {
             id: data.id,
             keyName: data.keyName,
-            value: data.value
+            value: data.value,
+            homePageUrl: data.homePageUrl,
+            logoUrl: data.logoUrl,
+            primaryColor: data.primaryColor,
+            contrastColor: data.contrastColor,
         };
         return result;
     }
