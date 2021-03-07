92ac02819c02d3e72f5a57fe219fe9788d2dae24
pranavmalvawala <pranavmalvawala@gmail.com>
2021-03-07 11:33:25 +0530

    revert earlier changes related to settings table

--- models/Setting.ts
+++ models/Setting.ts
@@ -3,8 +3,4 @@ export class Setting {
     public churchId?: string;
     public keyName?: string;
     public value?: string;
-    public homePageUrl?: string;
-    public logoUrl?: string;
-    public primaryColor?: string;
-    public contrastColor?: string;
 }
