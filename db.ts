6399067c1b9488e65a88b98e485a2ebb6df4a762
Jeremy Zongker <jeremy@trilitech.com>
2021-04-26 20:49:30 +0000

    Simplified db logic

--- db.ts
+++ db.ts
@@ -1,41 +1,41 @@
 import { Pool } from "./pool";
-import { PoolConnection, MysqlError } from "mysql";
+import { PoolConnection, MysqlError, queryCallback } from "mysql";
 import { LoggingHelper } from "./helpers";
 
 export class DB {
 
-    public static async usePooledConnectionAsync(actionAsync: (connect: PoolConnection) => any) {
-        const connection: PoolConnection = await new Promise((resolve, reject) => {
+    // wraps in promise
+    static async getConnection() {
+        const promise: Promise<PoolConnection> = new Promise((resolve, reject) => {
             Pool.current.getConnection((ex: MysqlError, conn: PoolConnection) => { if (ex) reject(ex); else resolve(conn); });
-        });
-        try {
-            return await actionAsync(connection);
-        } catch (ex) {
-            LoggingHelper.getCurrent().error(ex);
-        } finally {
-            connection.release();
-        }
+        });;
+        const connection: PoolConnection = await promise;
+        return connection;
     }
 
-    public static async queryOne(sql: string, params: any[]) {
-        return this.query(sql, params).then((result: any[]) => {
-            return result.length > 0 ? result[0] : null;
+    // wraps in promise
+    static async getQuery(connection: PoolConnection, sql: string, params: any[]) {
+        const promise: Promise<queryCallback> = new Promise((resolve, reject) => {
+            connection.query(sql, params, async (ex, rows) => {
+                if (ex) { LoggingHelper.getCurrent().error(ex); reject(ex); }
+                else { resolve(rows); }
+            });
         });
+        const query: queryCallback = await promise;
+        return query;
     }
 
     public static async query(sql: string, params: any[]) {
-        return this.usePooledConnectionAsync(async (connection) => {
-            const result: any[] = await new Promise((resolve, reject) => {
-                connection.query(sql, params, async (ex, rows) => {
-                    if (ex) { LoggingHelper.getCurrent().error(ex); reject(ex); }
-                    else {
-                        // LoggingHelper.getCurrent().info(rows);
-                        resolve(rows);
-                    }
-                });
+        let result: any = null;
+        const connection = await this.getConnection();
+        try { result = await this.getQuery(connection, sql, params); }
+        catch (ex) { LoggingHelper.getCurrent().error(ex); }
+        finally { connection.release(); }
+        return result;
+    }
 
-            });
-            return result;
-        });
+    public static async queryOne(sql: string, params: any[]) {
+        const result: any = this.query(sql, params);
+        return result.length > 0 ? result[0] : null;
     }
 }
