ae2df72d630d1e21b14f2bbe508dac1a343a0c4d
Jeremy Zongker <jeremy@trilitech.com>
2021-06-30 00:41:59 -0500

    Added ability to remove folders

--- helpers/FileHelper.ts
+++ helpers/FileHelper.ts
@@ -3,31 +3,42 @@ import fs from "fs";
 import path from "path";
 
 export class FileHelper {
-    private static fileStore = process.env.FILE_STORE;
-    private static rootPath = path.resolve("./content") + "/";
+  private static fileStore = process.env.FILE_STORE;
+  private static rootPath = path.resolve("./content") + "/";
 
-    static store = async (key: string, contentType: string, contents: Buffer) => {
-        switch (FileHelper.fileStore) {
-            case "S3": await AwsHelper.S3Upload(key, contentType, contents); break;
-            default: await FileHelper.storeLocal(key, contents); break;
-        }
+  static store = async (key: string, contentType: string, contents: Buffer) => {
+    switch (FileHelper.fileStore) {
+      case "S3": await AwsHelper.S3Upload(key, contentType, contents); break;
+      default: await FileHelper.storeLocal(key, contents); break;
     }
+  }
 
-    static remove = async (key: string) => {
-        switch (FileHelper.fileStore) {
-            case "S3": await AwsHelper.S3Remove(key); break;
-            default: await FileHelper.removeLocal(key); break;
-        }
+  static remove = async (key: string) => {
+    switch (FileHelper.fileStore) {
+      case "S3": await AwsHelper.S3Remove(key); break;
+      default: await FileHelper.removeLocal(key); break;
     }
+  }
 
-    private static storeLocal = async (key: string, contents: Buffer) => {
-        const fileName = FileHelper.rootPath + key;
-        const dirName = path.dirname(fileName);
-        if (!fs.existsSync(dirName)) fs.mkdirSync(dirName, { recursive: true });
-        fs.writeFileSync(fileName, contents);
+  static removeFolder = async (key: string) => {
+    switch (FileHelper.fileStore) {
+      case "S3": break; // no need on s3
+      default: await FileHelper.removeLocalFolder(key); break;
     }
+  }
 
-    private static removeLocal = async (key: string) => {
-        fs.unlinkSync(FileHelper.rootPath + key);
-    }
+  private static storeLocal = async (key: string, contents: Buffer) => {
+    const fileName = FileHelper.rootPath + key;
+    const dirName = path.dirname(fileName);
+    if (!fs.existsSync(dirName)) fs.mkdirSync(dirName, { recursive: true });
+    fs.writeFileSync(fileName, contents);
+  }
+
+  private static removeLocal = async (key: string) => {
+    fs.unlinkSync(FileHelper.rootPath + key);
+  }
+
+  private static removeLocalFolder = async (key: string) => {
+    fs.rmdirSync(FileHelper.rootPath + key);
+  }
 }
