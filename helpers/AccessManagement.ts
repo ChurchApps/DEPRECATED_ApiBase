3128c36498bdedce919582215b8a6b6d50b636a1
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-07-07 17:42:57 +0530

    replace displayName with firstName and lastName in userInterface

--- helpers/AccessManagement.ts
+++ helpers/AccessManagement.ts
@@ -2,7 +2,6 @@
 export interface ApplicationInterface { name: string, permissions: RolePermissionInterface[] }
 export interface ChurchInterface { id?: number, name: string, registrationDate?: Date, apps?: ApplicationInterface[] }
 export interface LoginResponseInterface { user: UserInterface, churches: ChurchInterface[], token: string, errors: string[] }
-export interface RegisterInterface { churchName?: string, displayName?: string, email?: string, password?: string }
 export interface RoleInterface { id?: number, churchId?: number, appName?: string, name?: string }
 export interface RolePermissionInterface { id?: number, churchId?: number, roleId?: number, appName?: string, contentType?: string, contentId?: number, action?: string }
 export interface RoleMemberInterface { id?: number, churchId?: number, roleId?: number, userId?: number, user?: UserInterface }
@@ -10,4 +9,4 @@ export interface ResetPasswordRequestInterface { userEmail: string, fromEmail: s
 export interface ResetPasswordResponseInterface { emailed: boolean }
 export interface SwitchAppRequestInterface { appName: string, churchId: number }
 export interface SwitchAppResponseInterface { appName: string, churchId: number }
-export interface UserInterface { id?: number, email?: string, authGuid?: string, displayName?: string, registrationDate?: Date, lastLogin?: Date, password?: string }
+export interface UserInterface { id?: number, email?: string, authGuid?: string, firstName?: string, lastName: string, registrationDate?: Date, lastLogin?: Date, password?: string }
