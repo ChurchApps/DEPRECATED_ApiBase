37849a8f1e1c2e99f190f0ca48678b70d3ac3657
Jeremy Zongker <jeremy@trilitech.com>
2021-08-09 16:31:14 +0000

    Added smtp email support.

--- helpers/EmailHelper.ts
+++ helpers/EmailHelper.ts
@@ -4,28 +4,38 @@ import directTransport from 'nodemailer-direct-transport'
 import { IEmailPayload } from '.'
 
 export class EmailHelper {
-    private static _mailSystem: string = process.env.MAIL_SYSTEM
+  private static _mailSystem: string = process.env.MAIL_SYSTEM
 
-    public static sendEmail({ from, to, subject, body }: IEmailPayload) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                let transporter: nodemailer.Transporter = nodemailer.createTransport(directTransport({
-                    name: 'churchapps.org'
-                }));
+  public static sendEmail({ from, to, subject, body }: IEmailPayload) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let transporter: nodemailer.Transporter = nodemailer.createTransport(directTransport({
+          name: "churchapps.org"
+        }));
 
-                if (this._mailSystem === 'SES') {
-                    AWS.config.update({ region: 'us-east-2' });
-                    const ses = new AWS.SES({ apiVersion: '2010-12-01' });
-                    transporter = nodemailer.createTransport({ SES: { ses, aws: AWS } })
-                }
-
-                await transporter.sendMail({ from, to, subject, html: body });
-                resolve(null);
-            } catch (err) {
-                reject(err);
+        if (this._mailSystem === 'SES') {
+          AWS.config.update({ region: 'us-east-2' });
+          const ses = new AWS.SES({ apiVersion: '2010-12-01' });
+          transporter = nodemailer.createTransport({ SES: { ses, aws: AWS } })
+        }
+        if (this._mailSystem === "SMTP") {
+          transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            secure: process.env.SMTP_SECURE === "true",
+            auth: {
+              user: process.env.SMTP_USER,
+              pass: process.env.SMTP_PASS
             }
+          });
+        }
 
-        })
-    }
+        await transporter.sendMail({ from, to, subject, html: body });
+        resolve(null);
+      } catch (err) {
+        reject(err);
+      }
+
+    })
+  }
 
 }
