4ddba2a69d1ecfd6d7ccb3f5564e3a6397aecd77
Jeremy Zongker <jeremy@trilitech.com>
2022-01-03 18:40:47 -0600

    Removed logging.

--- controllers/PageController.ts
+++ controllers/PageController.ts
@@ -7,71 +7,67 @@ import { Permissions, FileHelper, UniqueIdHelper } from "../helpers";
 @controller("/pages")
 export class PageController extends CustomBaseController {
 
-    @httpGet("/")
-    public async loadAll(req: express.Request, res: express.Response): Promise<any> {
-        return this.actionWrapper(req, res, async (au) => {
-            return await this.baseRepositories.page.loadAll(au.churchId);
-        });
-    }
+  @httpGet("/")
+  public async loadAll(req: express.Request, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      return await this.baseRepositories.page.loadAll(au.churchId);
+    });
+  }
 
-    @httpGet("/:churchId/:id")
-    public async loadAnon(@requestParam("churchId") churchId: string, @requestParam("id") id: string, req: express.Request, res: express.Response): Promise<any> {
-        return this.actionWrapperAnon(req, res, async () => {
-            const result = await this.baseRepositories.page.loadById(id, churchId);
-            return result;
-        });
-    }
+  @httpGet("/:churchId/:id")
+  public async loadAnon(@requestParam("churchId") churchId: string, @requestParam("id") id: string, req: express.Request, res: express.Response): Promise<any> {
+    return this.actionWrapperAnon(req, res, async () => {
+      const result = await this.baseRepositories.page.loadById(id, churchId);
+      return result;
+    });
+  }
 
-    @httpGet("/:id")
-    public async load(@requestParam("id") id: string, req: express.Request, res: express.Response): Promise<any> {
-        return this.actionWrapper(req, res, async (au) => {
-            const result = await this.baseRepositories.page.loadById(id, au.churchId);
-            return result;
-        });
-    }
+  @httpGet("/:id")
+  public async load(@requestParam("id") id: string, req: express.Request, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      const result = await this.baseRepositories.page.loadById(id, au.churchId);
+      return result;
+    });
+  }
 
-    @httpPost("/")
-    public async save(req: express.Request<{}, {}, Page[]>, res: express.Response): Promise<any> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.pages.edit)) return this.json({}, 401);
-            else {
-                let pages: Page[] = req.body;
-                const promises: Promise<Page>[] = [];
-                pages.forEach((page) => {
-                    if (page.churchId === au.churchId) promises.push(
-                        this.baseRepositories.page.save(page).then(async (p) => {
-                            if (page.content !== null && page.content !== "") {
-                                const buffer = Buffer.from(page.content, "binary");
-                                const path = au.churchId + "/pages/" + page.id + ".html";
-                                await FileHelper.store(path, "text/html", buffer);
-                                console.log("saved")
-                                console.log(page.path);
-                                if (UniqueIdHelper.isMissing(page.path)) {
-                                    console.log("saving path")
-                                    page.path = path;
-                                    await this.baseRepositories.page.save(page);
-                                }
-                                console.log(path);
-                            }
-                            return p;
-                        })
-                    )
-                });
-                pages = await Promise.all(promises);
-                return this.json(pages, 200);
-            }
+  @httpPost("/")
+  public async save(req: express.Request<{}, {}, Page[]>, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.pages.edit)) return this.json({}, 401);
+      else {
+        let pages: Page[] = req.body;
+        const promises: Promise<Page>[] = [];
+        pages.forEach((page) => {
+          if (page.churchId === au.churchId) promises.push(
+            this.baseRepositories.page.save(page).then(async (p) => {
+              if (page.content !== null && page.content !== "") {
+                const buffer = Buffer.from(page.content, "binary");
+                const path = au.churchId + "/pages/" + page.id + ".html";
+                await FileHelper.store(path, "text/html", buffer);
+                if (UniqueIdHelper.isMissing(page.path)) {
+                  page.path = path;
+                  await this.baseRepositories.page.save(page);
+                }
+              }
+              return p;
+            })
+          )
         });
-    }
+        pages = await Promise.all(promises);
+        return this.json(pages, 200);
+      }
+    });
+  }
 
-    @httpDelete("/:id")
-    public async delete(@requestParam("id") id: string, req: express.Request, res: express.Response): Promise<void> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.pages.edit)) return this.json({}, 401);
-            else {
-                await this.baseRepositories.page.delete(id, au.churchId);
-                await FileHelper.remove(au.churchId + "/pages/" + id + ".html");
-            }
-        });
-    }
+  @httpDelete("/:id")
+  public async delete(@requestParam("id") id: string, req: express.Request, res: express.Response): Promise<void> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.pages.edit)) return this.json({}, 401);
+      else {
+        await this.baseRepositories.page.delete(id, au.churchId);
+        await FileHelper.remove(au.churchId + "/pages/" + id + ".html");
+      }
+    });
+  }
 
 }
