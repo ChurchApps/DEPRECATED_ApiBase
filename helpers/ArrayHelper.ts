30f22557a54dd83aa9773b569dba84585a36783f
Jeremy Zongker <jeremy@trilitech.com>
2022-05-16 12:05:13 -0500

    Removed dependency on Person model

--- helpers/ArrayHelper.ts
+++ helpers/ArrayHelper.ts
@@ -1,29 +1,65 @@
 import { UniqueIdHelper } from "./UniqueIdHelper";
 
 export class ArrayHelper {
-    static getIds(array: any[], propertyName: string) {
-        const result: string[] = [];
-        for (const item of array) {
-            const id = item[propertyName];
-            if (!UniqueIdHelper.isMissing(id) && result.indexOf(id) === -1) result.push(id);
-        }
-        return result;
+  static getIds(array: any[], propertyName: string) {
+    const result: string[] = [];
+    for (const item of array) {
+      const id = item[propertyName];
+      if (!UniqueIdHelper.isMissing(id) && result.indexOf(id) === -1) result.push(id);
     }
+    return result;
+  }
 
-    static getOne(array: any[], propertyName: string, value: any) {
-        for (const item of array) if (item[propertyName] === value) return item;
-        return null
-    }
+  static getOne(array: any[], propertyName: string, value: any) {
+    for (const item of array) if (item[propertyName] === value) return item;
+    return null
+  }
 
-    static getAll(array: any[], propertyName: string, value: any) {
-        const result: any[] = []
-        for (const item of array) if (item[propertyName] === value) result.push(item);
-        return result;
-    }
+  static getAll(array: any[], propertyName: string, value: any) {
+    const result: any[] = []
+    for (const item of array) if (item[propertyName] === value) result.push(item);
+    return result;
+  }
 
-    static fillArray(contents: string, length: number) {
-        const result = [];
-        for (let i = 0; i < length; i++) result.push(contents);
-        return result;
+  static getAllOperator(array: any[], propertyName: string, value: any, operator: string) {
+    const result: any[] = []
+    for (const item of array) {
+      switch (operator) {
+        case "equals":
+          if (item[propertyName] === value) result.push(item);
+          break;
+        case "startsWith":
+          if (item[propertyName].indexOf(value) === 0) result.push(item);
+          break;
+        case "endsWith":
+          if (item[propertyName].indexOf(value) === item[propertyName].length - value.length) result.push(item);
+          break;
+        case "contains":
+          if (item[propertyName].indexOf(value) > -1) result.push(item);
+          break;
+        case "greaterThan":
+          if (item[propertyName] > value) result.push(item);
+          break;
+        case "greaterThanEqual":
+          if (item[propertyName] >= value) result.push(item);
+          break;
+        case "lessThan":
+          if (item[propertyName] < value) result.push(item);
+          break;
+        case "lessThanEqual":
+          if (item[propertyName] <= value) result.push(item);
+          break;
+        case "notEqual":
+          if (item[propertyName] != value) result.push(item);
+          break;
+      }
     }
+    return result;
+  }
+
+  static fillArray(contents: string, length: number) {
+    const result = [];
+    for (let i = 0; i < length; i++) result.push(contents);
+    return result;
+  }
 }
--- helpers/PersonHelper.ts
+++ helpers/PersonHelper.ts
@@ -1,12 +1,10 @@
-import { Person } from "../../models/Person"
-
 export class PersonHelper {
-  public static getPhotoUrl(churchId: string, person: Person) {
+  public static getPhotoUrl(churchId: string, person: any) {
     if (!person.photoUpdated) return "";
     else return "/" + churchId + "/membership/people/" + person.id + ".png?dt=" + person.photoUpdated.getTime().toString();
   }
 
-  public static getDisplayName(person: Person) {
+  public static getDisplayName(person: any) {
     if (person?.name?.nick !== null && person?.name?.nick !== "" && person?.name?.nick !== undefined) return person.name.first + " \"" + person.name.nick + "\" " + person.name.last;
     else return person.name.first + " " + person.name.last;
   }
