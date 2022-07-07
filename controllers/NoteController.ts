ab73a535c332e37e8c98ac61e7ed1c0f341b1bec
Jeremy Zongker <jeremy@trilitech.com>
2022-07-06 23:05:08 -0500

    Added no permission option to notes for DoingApi

--- controllers/NoteController.ts
+++ controllers/NoteController.ts
@@ -7,51 +7,51 @@ import { Permissions } from "../helpers";
 @controller("/notes")
 export class NoteController extends CustomBaseController {
 
-    @httpGet("/:id")
-    public async get(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.notes.view)) return this.json({}, 401);
-            else return this.baseRepositories.note.convertToModel(au.churchId, await this.baseRepositories.note.load(au.churchId, id));
-        });
-    }
+  @httpGet("/:id")
+  public async get(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.notes.view) && process.env.NO_NOTES_PERMISSION !== "true") return this.json({}, 401);
+      else return this.baseRepositories.note.convertToModel(au.churchId, await this.baseRepositories.note.load(au.churchId, id));
+    });
+  }
 
-    @httpGet("/:contentType/:contentId")
-    public async getForContent(@requestParam("contentType") contentType: string, @requestParam("contentId") contentId: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.notes.view)) return this.json({}, 401);
-            else return await this.baseRepositories.note.loadForContent(au.churchId, contentType, contentId).then(data => {
-                return this.baseRepositories.note.convertAllToModel(au.churchId, data)
-            });
-        });
-    }
+  @httpGet("/:contentType/:contentId")
+  public async getForContent(@requestParam("contentType") contentType: string, @requestParam("contentId") contentId: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.notes.view) && process.env.NO_NOTES_PERMISSION !== "true") return this.json({}, 401);
+      else return await this.baseRepositories.note.loadForContent(au.churchId, contentType, contentId).then(data => {
+        return this.baseRepositories.note.convertAllToModel(au.churchId, data)
+      });
+    });
+  }
 
-    @httpGet("/")
-    public async getAll(req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.notes.view)) return this.json({}, 401);
-            else return this.baseRepositories.note.convertAllToModel(au.churchId, await this.baseRepositories.note.loadAll(au.churchId));
-        });
-    }
+  @httpGet("/")
+  public async getAll(req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.notes.view) && process.env.NO_NOTES_PERMISSION !== "true") return this.json({}, 401);
+      else return this.baseRepositories.note.convertAllToModel(au.churchId, await this.baseRepositories.note.loadAll(au.churchId));
+    });
+  }
 
-    @httpPost("/")
-    public async save(req: express.Request<{}, {}, Note[]>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.notes.edit)) return this.json({}, 401);
-            else {
-                const promises: Promise<Note>[] = [];
-                req.body.forEach(note => { note.churchId = au.churchId; note.addedBy = au.personId; promises.push(this.baseRepositories.note.save(note)); });
-                const result = await Promise.all(promises);
-                return this.baseRepositories.note.convertAllToModel(au.churchId, result);
-            }
-        });
-    }
+  @httpPost("/")
+  public async save(req: express.Request<{}, {}, Note[]>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.notes.edit) && process.env.NO_NOTES_PERMISSION !== "true") return this.json({}, 401);
+      else {
+        const promises: Promise<Note>[] = [];
+        req.body.forEach(note => { note.churchId = au.churchId; note.addedBy = au.personId; promises.push(this.baseRepositories.note.save(note)); });
+        const result = await Promise.all(promises);
+        return this.baseRepositories.note.convertAllToModel(au.churchId, result);
+      }
+    });
+  }
 
-    @httpDelete("/:id")
-    public async delete(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.notes.edit)) return this.json({}, 401);
-            else await this.baseRepositories.note.delete(au.churchId, id);
-        });
-    }
+  @httpDelete("/:id")
+  public async delete(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.notes.edit) && process.env.NO_NOTES_PERMISSION !== "true") return this.json({}, 401);
+      else await this.baseRepositories.note.delete(au.churchId, id);
+    });
+  }
 
 }
